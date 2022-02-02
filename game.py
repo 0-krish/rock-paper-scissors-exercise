682bbbd3006086d5bc023ef0a66c37468756c5de
0-krish <65729597+0-krish@users.noreply.github.com>
2022-02-02 18:03:41 -0500

    Update game.py

--- game.py
+++ game.py
@@ -1,1 +1,25 @@
-print("Rock, Paper, Scissors, Shoot!")
+# Rock-Paper-Scissors Exercise
+#
+# Author: Krish Sarawgi
+#   Github: "0-krish"
+#   NetID: ks1730
+
+# Introducing the Game
+print("Welcome to the Rock-Paper-Scissors Game! Lets get started.")
+
+# Processing User Inputs
+print("Play your hand: rock, paper, or scissors?")
+user_input = input()
+
+# Validating User Inputs
+
+# Simulating Computer Selection
+
+# Determining the Winner
+
+# Displaying Results
+
+
+
+
+
