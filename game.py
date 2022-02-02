dd5cf8366b4e8571184df8381157d6b08fd9bf05
0-krish <65729597+0-krish@users.noreply.github.com>
2022-02-02 18:15:45 -0500

    Validating user input

--- game.py
+++ game.py
@@ -12,6 +12,17 @@ print("Play your hand: rock, paper, or scissors?")
 user_input = input()
 
 # Validating User Inputs
+validated_user_input = user_input.lower()
+if ((validated_user_input != "rock") and
+    (validated_user_input != "paper") and
+    (validated_user_input != "scissors")):
+
+    print("Sorry! Invalid input detected.")
+    print("This game accepts only three inputs:",
+          "rock, paper, and scissors")
+    print("This game will now quit.")
+
+    exit()
 
 # Simulating Computer Selection
 
